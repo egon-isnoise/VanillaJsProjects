aaaad317e5049d7a0dc828940879cad4e5a40543
Andrea Allegri <44090143+egonmakesnoise@users.noreply.github.com>
2020-10-01 16:40:44 +0200

    Add files via upload

---
+++ Sidebar/dist/app.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var toggleBtn = document.querySelector(".sidebar-toggle");
+var closeBtn = document.querySelector(".close-btn");
+var sidebar = document.querySelector(".sidebar");
+toggleBtn.addEventListener('click', function () {
+  //console.log(sidebar.classList);
+  //if(sidebar.classList.contains("show-sidebar")){
+  //    sidebar.classList.remove("show-sidebar");
+  //}else{
+  //    sidebar.classList.add("show-sidebar");
+  //}
+  sidebar.classList.toggle("show-sidebar");
+});
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove("show-sidebar");
+});
